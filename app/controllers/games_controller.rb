71ca1f06a7c6b72581cd646db561624bfc9146f5
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-11 14:19:47 +0100

    backup to github

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+
+def new
+
+
+  # new random grid and a form
+end
+
+def score
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'about', to: 'pages#about'
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action'
+end
+
+  # The form will be submitted (with POST) to the score action.
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
